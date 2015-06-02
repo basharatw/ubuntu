fa0aa8c967de197c79405cfcec2f6a8abdb80cea
Basharat Wani <basharatw@gmail.com>
2015-06-02 13:36:13 -0400

    Code file of example 4

---
+++ ex4.py
@@ -0,0 +1,77 @@
+#Example 4
+
+#Total cars 
+cars = 100
+#Space in a car
+space_in_a_car = 4.0
+print space_in_a_car
+print "=======4.0========"
+
+space_in_a_car = 4
+print space_in_a_car
+print "========= 4 ============"
+
+#Total drivers
+drivers = 30
+
+#Total passangers 
+passengers = 90
+
+cars_not_driven = cars - drivers
+print cars_not_driven
+
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+print "Average Passengers per car", average_passengers_per_car
+print "=================="
+
+print "There are", cars, "cars available."
+print "There are only", drivers, "drivers available."
+print space_in_a_car
+
+
+#Total drivers
+drivers = 30
+
+#Total passangers 
+passengers = 90
+
+cars_not_driven = cars - drivers
+print cars_not_driven
+
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+print "Average Passengers per car", average_passengers_per_car
+print "=================="
+
+print "There are", cars, "cars available."
+print "There are only", drivers, "drivers available."
+
+#Total drivers
+drivers = 30
+
+#Total passangers 
+passengers = 90
+
+cars_not_driven = cars - drivers
+print cars_not_driven
+
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+print "Average Passengers per car", average_passengers_per_car
+print "=================="
+
+print "There are", cars, "cars available."
+print "There are only", drivers, "drivers available."
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car."
+
+
