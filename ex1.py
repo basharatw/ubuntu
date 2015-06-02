4acee0a340174af19ae8a074400a31ec4158bdd6
Basharat Wani <basharatw@gmail.com>
2015-06-02 10:31:18 -0400

    First Code Check In from ubuntu 14x

---
+++ ex1.py
@@ -0,0 +1,12 @@
+print ""
+print "==============================="
+print "Basharat Added this Project"
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "==============================="
+
