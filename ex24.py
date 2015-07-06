048e30413c351a774da6eed9f0ab5ae2fd400019
Basharat Wani <basharatw@gmail.com>
2015-07-06 14:05:33 -0400

    More Python Code

--- ex24.py
+++ ex24.py
@@ -27,17 +27,17 @@ def secret_formula(started):
     return jelly_beans, jars, crates
 
 
-start_point = 10000/2
+start_point = 10000
 beans, jars, crates = secret_formula(start_point)
 
 print "With a starting point of: %d" % start_point
 print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
 
-#start_point = start_point / 10
+start_point = start_point / 10
 
-#print "We can also do that this way:"
+print "We can also do that this way:"
 #print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
-# as of Thu Jun 25 13:54:2
+print secret_formula(start_point)
 
 
 
---
+++ ex25a.py
@@ -0,0 +1,25 @@
+# Test File for ex25.py
+
+import ex25
+sentence = "All good things come to those who wait."
+words = ex25.break_words(sentence)
+print words
+
+
+sorted_words = ex25.sort_words(words)
+sorted_words
+print sorted_words
+
+ex25.print_first_word(words)
+ex25.print_last_word(words)
+
+print "Sorted Word"
+ex25.print_first_word(sorted_words)
+ex25.print_last_word(sorted_words)
+sorted_words
+
+sorted_words = ex25.sort_sentence(sentence)
+sorted_words
+ex25.print_first_and_last(sentence)
+ex25.print_first_and_last_sorted(sentence)
+
